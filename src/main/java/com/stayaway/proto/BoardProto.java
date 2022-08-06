0b67676439b3c5b6a62a6db1e0ca9e47ccace3e2
tilacyn <tilacyn@yandex-team.ru>
2022-08-06 11:15:22 +0300

    [STAYAWAYBACK-6] removed List<PlayerBroto> from BoardProto

--- src/main/java/com/stayaway/proto/BoardProto.java
+++ src/main/java/com/stayaway/proto/BoardProto.java
@@ -1,15 +1,17 @@
 package com.stayaway.proto;
 
+import com.stayaway.cards.Card;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
 public class BoardProto {
-    private List<PlayerProto> players;
+    private List<String> players;
     private CardTypeProto topDeckCardType;
 //    should be empty if user is not authorized to see which player the thing is
     private String theThingUserID;
     private String currentPlayerUserID;
     private MoveInProgressProto playCardStatus;
+    List<Card> cards;
 }
